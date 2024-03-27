94bb601a6da73f28297f897ab1fb7f751a97257b
Pedro H. Oldoni <144467616+rickxyo@users.noreply.github.com>
2024-03-26 21:14:03 -0300

    Strict Mode

---
+++ starter/script.js
@@ -0,0 +1,8 @@
+// Activating Strict Mode
+"use strict";
+
+let hasDriversLicense = false;
+const passTest = true;
+
+if (passTest) hasDriversLicense = true;
+if (hasDriversLicense) console.log("i can drive");
