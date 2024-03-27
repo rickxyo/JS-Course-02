5ef463c86c1ebff36720afe2a1a7a5011c837267
Pedro H. Oldoni <144467616+rickxyo@users.noreply.github.com>
2024-03-26 21:41:05 -0300

    Functions

--- starter/script.js
+++ starter/script.js
@@ -1,8 +1,27 @@
 // Activating Strict Mode
-"use strict";
+// "use strict";
 
-let hasDriversLicense = false;
-const passTest = true;
+// let hasDriversLicense = false;
+// const passTest = true;
 
-if (passTest) hasDriversLicense = true;
-if (hasDriversLicense) console.log("i can drive");
+// if (passTest) hasDriversLicense = true;
+// if (hasDriversLicense) console.log("i can drive");
+
+// Functions
+function logger() {
+  console.log("My name is Pedro");
+}
+
+// calling / running / invoking the function
+logger();
+
+function fruitProcessor(apples, oranges) {
+  const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+  return juice;
+}
+
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+
+const appleOrangeJuice = fruitProcessor(2, 4);
+console.log(appleOrangeJuice);
