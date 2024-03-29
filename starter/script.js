66342c7a18c1bc96fba6900a9970b04c981c3b45
Pedro H. Oldoni <144467616+rickxyo@users.noreply.github.com>
2024-03-29 18:40:02 -0300

    function declaration vs expression

--- starter/script.js
+++ starter/script.js
@@ -8,20 +8,34 @@
 // if (hasDriversLicense) console.log("i can drive");
 
 // Functions
-function logger() {
-  console.log("My name is Pedro");
+// function logger() {
+//   console.log("My name is Pedro");
+// }
+
+// // calling / running / invoking the function
+// logger();
+
+// function fruitProcessor(apples, oranges) {
+//   const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+//   return juice;
+// }
+
+// const appleJuice = fruitProcessor(5, 0);
+// console.log(appleJuice);
+
+// const appleOrangeJuice = fruitProcessor(2, 4);
+// console.log(appleOrangeJuice);
+
+// function declaration
+function calcAge1(birthYear) {
+  return 2037 - birthYear;
 }
+const age1 = calcAge1(1991);
 
-// calling / running / invoking the function
-logger();
+// function expression
+const calcAge2 = function (birthYear) {
+  return 2037 - birthYear;
+};
+const age2 = calcAge2(1991);
 
-function fruitProcessor(apples, oranges) {
-  const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
-  return juice;
-}
-
-const appleJuice = fruitProcessor(5, 0);
-console.log(appleJuice);
-
-const appleOrangeJuice = fruitProcessor(2, 4);
-console.log(appleOrangeJuice);
+console.log(age1, age2);
