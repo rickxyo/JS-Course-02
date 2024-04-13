f35ba535a4eb903102f40562959f99d4480b0944
Pedro H. Oldoni <144467616+rickxyo@users.noreply.github.com>
2024-04-13 11:01:03 -0300

    Arrow Function

--- starter/script.js
+++ starter/script.js
@@ -27,15 +27,31 @@
 // console.log(appleOrangeJuice);
 
 // function declaration
-function calcAge1(birthYear) {
-  return 2037 - birthYear;
-}
-const age1 = calcAge1(1991);
+// function calcAge1(birthYear) {
+//   return 2037 - birthYear;
+// }
+// const age1 = calcAge1(1991);
 
-// function expression
-const calcAge2 = function (birthYear) {
-  return 2037 - birthYear;
+// // function expression
+// const calcAge2 = function (birthYear) {
+//   return 2037 - birthYear;
+// };
+// const age2 = calcAge2(1991);
+
+// console.log(age1, age2);
+
+// Arrow function
+
+const calcAge3 = (birthYear) => 2037 - birthYear;
+const age3 = calcAge3(1991);
+console.log(age3);
+
+const yearsUntilRetirement = (birthYear, firstName) => {
+  const age = 2037 - birthYear;
+  const retirement = 65 - age;
+  // return retirement;
+  return `${firstName} retires in ${retirement} years`;
 };
-const age2 = calcAge2(1991);
 
-console.log(age1, age2);
+console.log(yearsUntilRetirement(1991, "Pedro"));
+console.log(yearsUntilRetirement(1991, "Bob"));
