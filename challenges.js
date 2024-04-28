3055ef98fdaeb49005f20f013e9669e165c9cf07
Pedro H. Oldoni <144467616+rickxyo@users.noreply.github.com>
2024-04-28 13:18:23 -0300

    challenge 03

--- challenges.js
+++ challenges.js
@@ -25,18 +25,57 @@
 // console.log(checkWinner(scoreKoalas, scoreDolphins));
 
 // Challenge #02
-const bill = Number;
+// const bill = Number;
 
-function calcTip(bill) {
-  const tip = bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
-  return tip;
+// function calcTip(bill) {
+//   const tip = bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+//   return tip;
+// }
+
+// // console.log (calcTip(100));
+// const tip1 = console.log(calcTip(125));
+// const tip2 = console.log(calcTip(555));
+// const tip3 = console.log(calcTip(44));
+
+// // const tips = [tip1, tip2, tip3];
+// const tips = [calcTip(125), calcTip(555), calcTip(44)];
+// const bills = [125, 555, 44];
+
+// Challenge #03
+/* Write your code below. Good luck! 🙂 */
+
+const mark = {
+  fullName: "Mark Miller",
+  mass: 78,
+  height: 1.69,
+
+  calcBMI: function () {
+    this.bmi = this.mass / (this.height * this.height);
+    return this.bmi;
+  },
+};
+
+const john = {
+  fullName: "John Smith",
+  mass: 92,
+  height: 1.95,
+
+  calcBMI: function () {
+    this.bmi = this.mass / (this.height * this.height);
+    return this.bmi;
+  },
+};
+
+if (mark.calcBMI() > john.calcBMI()) {
+  console.log(
+    `${mark.fullName}´s BMI (${mark.calcBMI()}) is higher than ${
+      john.fullName
+    }´s (${john.calcBMI()})!`
+  );
+} else {
+  console.log(
+    `${john.fullName}´s BMI (${john.calcBMI()}) is higher than ${
+      mark.fullName
+    }´s (${mark.calcBMI()})!`
+  );
 }
-
-// console.log (calcTip(100));
-const tip1 = console.log(calcTip(125));
-const tip2 = console.log(calcTip(555));
-const tip3 = console.log(calcTip(44));
-
-// const tips = [tip1, tip2, tip3];
-const tips = [calcTip(125), calcTip(555), calcTip(44)];
-const bills = [125, 555, 44];
